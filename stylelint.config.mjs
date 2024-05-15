aa6c889a9a5b549ded88b337d78e3c73c54bb517
arthur-plazanet <72502545+arthur-plazanet@users.noreply.github.com>
2024-05-15 22:16:05 +0200

    Integrate Basic Stylelint (#2)
    * Vue components export and import in external project
    
    * First draft basic stylelint integration
    
    * Remove ButtonCopy

---
+++ stylelint.config.mjs
@@ -0,0 +1,23 @@
+export default {
+  extends: ['stylelint-config-standard-scss', 'stylelint-config-recommended-vue'],
+
+  // TODO: V2?
+  // plugins: ['stylelint-declaration-block-no-ignored-properties'],
+  // rules: {
+  //   'declaration-block-no-duplicate-custom-properties': true,
+  //   'declaration-block-no-shorthand-property-overrides': true,
+  //   'custom-property-pattern': '^[a-z]+(-[a-z]+)*$',
+  //   'custom-property-no-missing-var-function': true,
+
+  //   'max-nesting-depth': 3,
+  //   'scss/dollar-variable-pattern': '^\\$[a-z]+(-[a-z]+)*$',
+  //   'scss/at-import-no-partial-leading-underscore': true,
+  //   'scss/at-function-pattern': '^\\$[a-z]+(-[a-z]+)*$',
+
+  //   'color-no-invalid-hex': true,
+  //   'font-family-no-duplicate-names': true,
+  //   'selector-pseudo-class-no-unknown': true,
+  //   'media-feature-name-no-unknown': true,
+  //   'property-no-unknown': true,
+  // },
+}
