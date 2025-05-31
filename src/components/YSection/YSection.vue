d98b485bf1f295746407c16cc2742aa43f3e8635
arthur-plazanet <72502545+arthur-plazanet@users.noreply.github.com>
2025-05-31 19:46:10 +0200

    Feature/vertical title and section (#292)
    * Fix state common lightness between variants
    
    * Adjust state base lightness
    
    * Vertical Title and Section
    
    * Fix build

---
+++ src/components/YSection/YSection.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="y-section">
+    <YVerticalTitle v-if="title">
+      {{ title }}
+    </YVerticalTitle>
+    <div class="y-section__content">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { YVerticalTitle } from '../YVerticalTitle'
+import './YSection.css'
+import './YSection.scss'
+
+interface Props {
+  title?: string
+}
+defineProps<Props>()
+</script>
