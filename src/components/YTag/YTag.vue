a5795d50a642a3b82f6bdf49f9f67690289e0bed
Arthur Plazanet <plazanet.art@gmail.com>
2025-06-09 02:12:34 +0200

    Generic Tag component

---
+++ src/components/YTag/YTag.vue
@@ -0,0 +1,24 @@
+<template>
+  <div :class="getClasses">
+    <slot>
+      <span>YTag</span>
+    </slot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { basePropsDefault, useBaseProps } from '@/composables/use-base-props'
+import { computed, defineProps, withDefaults } from 'vue'
+import type { YTagProps } from './types'
+import './YTag.scss'
+
+const props = withDefaults(defineProps<YTagProps>(), {
+  ...basePropsDefault,
+})
+
+const { baseClasses } = useBaseProps(props)
+
+const getClasses = computed(() => {
+  return [...baseClasses.value, 'y-tag']
+})
+</script>
